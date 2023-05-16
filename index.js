c22ad2a559e72744e37e798ddad9e7b7dcb1295d
JohnK <wilikiayomikun7@gmail.com>
2023-05-16 23:36:50 +0100

    Today's commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+
 console.log("This is my first javascript webpage");
 
 let x= 6;
 let y = 7;
 let sum = x + y;
 console. log(sum);
+
+
+
+/*Create a condition that will determine if a trader 
+makes a certain amount of profit in his or herbusiness*/
+
+let capTrader = 5000;
+let p = 4450;
+let profit = capTrader - p;
+if (capTrader - p) {
+    console.log(`The answer should be = ${capTrader - p}`);  
+} else{
+    console.log("This is absolutely wrong");
+}
