d27b47fab58a1b3362970308a773379a1374d02f
JohnK <wilikiayomikun7@gmail.com>
2023-05-17 01:28:49 +0100

    My new Commit.

--- index2.js
+++ index2.js
@@ -1,1 +1,34 @@
 console.log('Javascript External coding');
+
+let partyPlusOne = "This is the boy ";
+let partyPlusTwo = "everybody in the Campus is talking about.";
+console.log(partyPlusOne + partyPlusTwo); 
+
+let moneyMatter = "This is Yetunde and ";
+let money = "she is 32 years of age."
+console.log(moneyMatter + money);
+
+let j = 15;
+let k = 25;
+let l = 30;
+let m = 40;
+let n = 20;
+let o = 35;
+if (j * k - l + m / n * o) {
+    console.log(`The answer must be = ${j * k - l + m / n * o}`); 
+} else{
+    console.log(`It should show error`);
+}
+
+let num4 = 6;
+let num5 = 8;
+let outcome = num4 + num5;
+console.log(outcome);
+
+const userName = "JohnsonK7";
+console.log(`Hello ${userName}!`);
+
+let name = "Ayomikun";
+let companyName = "DevNaija Academy Limited."
+console.log(`${name} works at ${companyName}`);
+
