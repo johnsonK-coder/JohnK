179b7e6d1744d6602297158c38a70c38527e2d91
JohnK <wilikiayomikun7@gmail.com>
2023-05-18 13:22:52 +0100

    Combination

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 
-console.log("This is my first javascript webpage");
+// console.log("This is my first javascript webpage");
 
-let x= 6;
-let y = 7;
-let sum = x + y;
-console. log(sum);
+// let x= 6;
+// let y = 7;
+// let sum = x + y;
+// console. log(sum);
 
 
 
-/*Create a condition that will determine if a trader 
-makes a certain amount of profit in his or herbusiness*/
+/* Create a condition that will determine if a trader 
+makes a certain amount of profit in his or herbusiness */
 
 let capTrader = 5000;
 let p = 4450;
 let profit = capTrader - p;
 if (capTrader - p) {
-    console.log(`The answer should be = ${capTrader - p}`);  
+    console.log(`The trader makes a profit of ${capTrader - p} in his Trade.`);  
 } else{
     console.log("This is absolutely wrong");
 }
+
+
+    //   Basic salary = 50,000;
+
+      let housing = 15000;
+      let transport =10000;
+      let feeding = 20000;
+      let mainSalary = 100000
+      if(gross = housing + transport + feeding){
+        console.log("You are a Rich man");
+      }
+      else{
+        console.log("You are an average man");
+      }
+
+      for(let j = 1; j<= 12; j++){
+        console.log(`2 x ${j} = ${j * 2} `);
+      }
